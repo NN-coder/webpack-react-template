ebe45eb4d846af95d9eb55feab68231452b3e838
Nikita Naleikin <trubatvoia3@gmail.com>
2021-06-10 01:07:50 +0400

    Adds some webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const CaseSensitivePathsWebpackPlugin = require('case-sensitive-paths-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { EnvironmentPlugin, HotModuleReplacementPlugin } = require('webpack');
@@ -7,11 +10,13 @@ const { EnvironmentPlugin, HotModuleReplacementPlugin } = require('webpack');
 const { NODE_ENV } = process.env;
 const isDev = NODE_ENV === 'development';
 
+const resolve = (...pathSegments) => path.resolve(process.cwd(), ...pathSegments);
+
 module.exports = {
   mode: NODE_ENV,
-  entry: path.resolve(__dirname, 'src/index.tsx'),
+  entry: resolve('src/index.tsx'),
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: resolve('build'),
     filename: 'bundle.js',
   },
   resolve: {
@@ -33,8 +38,19 @@ module.exports = {
   plugins: [
     isDev && new HotModuleReplacementPlugin(),
     isDev && new ReactRefreshWebpackPlugin(),
+    new CaseSensitivePathsWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: resolve('public'),
+          globOptions: { ignore: resolve('public/index.html') },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
     new EnvironmentPlugin({ NODE_ENV }),
     new ForkTsCheckerWebpackPlugin(),
-    new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public/index.html') }),
+    new HtmlWebpackPlugin({ template: resolve('public/index.html') }),
   ].filter(Boolean),
 };
